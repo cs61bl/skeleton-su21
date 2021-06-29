4c1d68a4ee4f72949458c5a6d0c2b93826720d21
Allyson Park <allysonpark@berkeley.edu>
2021-06-28 17:38:16 -0700

    Release proj1 to students

---
+++ proj1/deque/LinkedListDequeTest.java
@@ -0,0 +1,38 @@
+package deque;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/** Performs some basic linked list deque tests. */
+public class LinkedListDequeTest {
+
+    /** You MUST use the variable below for all of your tests. If you test
+     * using a local variable, and not this static variable below, the
+     * autograder will not grade that test. If you would like to test
+     * LinkedListDeques with types other than Integer (and you should),
+     * you can define a new local variable. However, the autograder will
+     * not grade that test. */
+    public static Deque<Integer> lld;
+
+    @Test
+    /** Adds a few things to the list, checks that isEmpty() is correct.
+     * This is one simple test to remind you how junit tests work. You
+     * should write more tests of your own.
+     *
+     * && is the "and" operation. */
+    public void addIsEmptySizeTest() {
+
+        System.out.println("Make sure to uncomment the lines below (and delete this print statement)." +
+                " The code you submit to the AG shouldn't have any print statements!");
+        /*
+        lld = new LinkedListDeque<Integer>();
+
+		assertTrue("A newly initialized LLDeque should be empty", lld.isEmpty());
+		lld.addFirst(0);
+
+        assertFalse("lld1 should now contain 1 item", lld.isEmpty());
+		*/
+    }
+
+}
