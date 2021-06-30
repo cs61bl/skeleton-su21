dc0549b1f4fc0005e35cfe81a5d7b77475e10a9f
Sohum Hulyalkar <ssohum11@berkeley.edu>
2021-06-30 14:45:32 -0700

    added lab07g

---
+++ lab07/ExtraCodingChallenges.java
@@ -0,0 +1,23 @@
+public class ExtraCodingChallenges {
+
+    /**
+     * Note this problem is not required for the lab, and will not be tested 
+     * on Gradescope.
+     * 
+     * Return the string with the index closest to its duplicate in the array.
+     * If there are no duplicates in the array, return null.
+     * Given the strings,
+     *
+     *     "all", "work", "and", "no", "play", "makes", "for",
+     *     "no", "work", "no", "fun", "and", "no", "results"
+     *
+     * nearestDuplicate would return "no" as the second and third "no" are the
+     * closest duplicates based on their positions in the array. If there is a tie, 
+     * i.e. there are two strings equally close, you may return either. 
+     */
+    public static String nearestDuplicate(String[] words) {
+        int min = Integer.MAX_VALUE;
+        // TODO
+        return null;
+    }
+}
