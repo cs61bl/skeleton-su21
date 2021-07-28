8687afc3beacf1356f59bd00f1bcc348995bb21f
Sohum Hulyalkar <ssohum11@berkeley.edu>
2021-07-28 10:42:18 -0500

    fixing lab 17

--- lab17/SimpleNameMap.java
+++ lab17/SimpleNameMap.java
@@ -1,53 +1,9 @@
-import java.util.HashMap;
-
 public class SimpleNameMap {
 
     /* TODO: Instance variables here */
 
     public SimpleNameMap() {
         // TODO: YOUR CODE HERE
-
-
-    }
-
-    public static void main(String[] args) {
-Lamp a = new Lamp(1);  // a.brightness = 1
-Lamp b = new Lamp(2);  // b.brightness = 2
-
-HashMap<Lamp, Integer> map = new HashMap<>();
-
-map.put(b, 0);
-map.put(a, 1);
-map.put(a, 2);
-
-System.out.println(map.get(a));  // print statement 1
-System.out.println(map.get(b));  // print statement 2
-
-map.put(b, 3);
-a.brightness = 2;
-map.put(b, 4);
-"hello".substring()
-System.out.println(map.get(a));  // print statement 3
-System.out.println(map.get(b));  // print statement 4
-System.out.println(map.get(new Lamp(1)));
-    }
-
-    static class Lamp {
-        int brightness;
-
-        Lamp(int brightness) {
-            this.brightness = brightness;
-        }
-
-        @Override
-        public int hashCode() {
-            return brightness;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            return ((Lamp) o).brightness == brightness;
-        }
     }
 
     /* Returns the number of items contained in this map. */
